e3884906ccb004f42fa003f21956c34305c6fc17
Brook Vita <bachchinhtran@gmail.com>
2025-05-11 14:56:30 +0700

    init: init project with spring init

---
+++ src/main/java/com/example/webdev_intern/WebdevInternApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdev_intern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdevInternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdevInternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webdev_intern/WebdevInternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdev_intern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdevInternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
