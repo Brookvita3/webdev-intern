eea0032b00bb0a60d6042c81398e480a75ae3f70
Brook Vita <bachchinhtran@gmail.com>
2025-05-11 16:41:26 +0700

    feat(dataset): deploy DB to Aiven and configure connection in app

---
+++ src/main/resources/db/migration/V1__create_students_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE students (
+    sbd VARCHAR(20) PRIMARY KEY,
+    toan DOUBLE,
+    ngu_van DOUBLE,
+    ngoai_ngu DOUBLE,
+    vat_li DOUBLE,
+    hoa_hoc DOUBLE,
+    sinh_hoc DOUBLE,
+    lich_su DOUBLE,
+    dia_li DOUBLE,
+    gdcd DOUBLE,
+    ma_ngoai_ngu VARCHAR(10)
+);
