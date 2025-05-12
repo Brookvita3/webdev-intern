1c53ab3b75d6f650d85f6ac35a6caefa0993cd41
Brook Vita <bachchinhtran@gmail.com>
2025-05-12 23:43:37 +0700

    fix(app): allow cors for all origins

--- src/main/java/com/example/webdev_intern/config/WebMvcConfig.java
+++ src/main/java/com/example/webdev_intern/config/WebMvcConfig.java
@@ -10,10 +10,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5500")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(false);
     }
 
 }
